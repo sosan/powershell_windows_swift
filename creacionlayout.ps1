d76ca1876954f3dae39927783c9f34459e2e6d2f
jose luis <josloe@gmail.com>
2020-12-02 23:49:25 +0100

    creacion de cache local de visual studio 2019

---
+++ creacionlayout.ps1
@@ -0,0 +1,5 @@
+# creacion de un layout
+.\vs_community.exe --layout .\vslayoutswift "--add", "Component.CPython3.x64", "--add", "Microsoft.VisualStudio.Component.Git", 
+            "--add", "Microsoft.VisualStudio.Component.VC.ATL", "--add", "Microsoft.VisualStudio.Component.VC.CMake.Project", 
+            "--add", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64", "--add", "Microsoft.VisualStudio.Component.Windows10SDK",
+            "--add", "Microsoft.VisualStudio.Component.Windows10SDK.17763" --lang es-ES
